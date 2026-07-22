e01b8d986f7a6233961e7e8a7ef3840b6d41c081
Yayang <yayangkasep.id@gmail.com>
2026-07-23 00:40:45 +0700

    Fix gmail prompt import

--- config/prompts/__init__.py
+++ config/prompts/__init__.py
@@ -1,8 +1,6 @@
-# Package init
-
 from .base import BASE_PERSONA, STYLE_GUIDELINES, TOOL_USAGE_INTRO
 from .system_os import SYSTEM_OS_PROMPT
-from .gmail import GMAIL_PROMPT
+from .gmail import GMAIL_READ_PROMPT, GMAIL_SEND_PROMPT
 from .google_calendar import CALENDAR_PROMPT
 from .google_tasks import TASKS_PROMPT
 from .google_drive import DRIVE_PROMPT
@@ -16,7 +14,8 @@ __all__ = [
     "TOOL_USAGE_INTRO",
     "IMAGE_GEN_PROMPT",
     "SYSTEM_OS_PROMPT",
-    "GMAIL_PROMPT",
+    "GMAIL_READ_PROMPT",
+    "GMAIL_SEND_PROMPT",
     "CALENDAR_PROMPT",
     "TASKS_PROMPT",
     "DRIVE_PROMPT",
