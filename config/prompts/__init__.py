55081ed33b77eeb9c7996ac8ad947da6840bac49
Yayang <yayangkasep.id@gmail.com>
2026-07-23 00:39:39 +0700

    Fix import error in prompts init

--- config/prompts/__init__.py
+++ config/prompts/__init__.py
@@ -1,11 +1,12 @@
 # Package init
 
-from .base import BASE_PERSONA, STYLE_GUIDELINES, TOOL_USAGE_INTRO, IMAGE_GEN_PROMPT
+from .base import BASE_PERSONA, STYLE_GUIDELINES, TOOL_USAGE_INTRO
 from .system_os import SYSTEM_OS_PROMPT
 from .gmail import GMAIL_PROMPT
 from .google_calendar import CALENDAR_PROMPT
 from .google_tasks import TASKS_PROMPT
 from .google_drive import DRIVE_PROMPT
+from .image_gen import IMAGE_GEN_PROMPT
 from .web_search import WEB_SEARCH_PROMPT
 from .coding import CODING_PROMPT
 
